5c00fabf628b750a4095f2c39b75816636bdb9e1
stijn <prentsb@hva.nl>
2025-08-27 12:56:52 +0200

    Update `vercel.json` and API handler paths:
    - Adjust function path in `vercel.json` to `api/[...all].ts`
    - Fix import in `api/[...all].ts` to point to `src/app`

--- api/[...all].ts
+++ api/[...all].ts
@@ -1,6 +1,6 @@
 // api/[...all].ts
 import type { VercelRequest, VercelResponse } from "@vercel/node";
-import app from "../app"; // because app.ts is at the repo root
+import app from "../src/app"; // because app.ts is at the repo root
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
     // Express apps are request handlers; just pass the request to it.
